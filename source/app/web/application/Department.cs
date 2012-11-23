3f7fff79c9ecbb117177170081828be3aa40eade
herbchow <HerbertC@iqmetrix.com>
2012-11-23 09:37:28 -0800

    Committing

--- source/app/web/application/Department.cs
+++ source/app/web/application/Department.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace app.web.application
 {
     public class Department
     {
         public string name { get; set; }
         public bool has_sub_departments { get; set; }
+        public Guid departmentId { get; set; }
     }
 }
