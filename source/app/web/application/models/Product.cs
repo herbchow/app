31c2770eb68674f9f8a00a4ea8ec3ebc1f80f87b
herbchow <HerbertC@iqmetrix.com>
2012-11-23 12:09:06 -0800

    fix up build after merging

--- source/app/web/application/models/Product.cs
+++ source/app/web/application/models/Product.cs
@@ -3,5 +3,6 @@ namespace app.web.application.models
     public class Product
     {
         public string name { get; set; }
+        public int departmentId { get; set; }
     }
 }
--- source/app/web/core/ControllerRequestFactory.cs
+++ source/app/web/core/ControllerRequestFactory.cs
@@ -6,16 +6,18 @@ namespace app.web.core
     {
         private readonly IGetViewNameFromRequest view_name_parser;
         private readonly IGetActionNameFromRequest action_name_parser;
+        private readonly IGetParameterNameFromRequest parameter_name_parser;
 
-        public ControllerRequestFactory(IGetViewNameFromRequest view_name_parser, IGetActionNameFromRequest action_name_parser)
+        public ControllerRequestFactory(IGetViewNameFromRequest view_name_parser, IGetActionNameFromRequest action_name_parser, IGetParameterNameFromRequest parameter_name_parser)
         {
             this.view_name_parser = view_name_parser;
             this.action_name_parser = action_name_parser;
+            this.parameter_name_parser = parameter_name_parser;
         }
 
         public IContainRequestDetails create_a_controller_request_from(HttpContext a_raw_aspnet_request)
         {
-            return new RequestDetails(() => a_raw_aspnet_request, view_name_parser, action_name_parser);
+            return new RequestDetails(() => a_raw_aspnet_request, view_name_parser, action_name_parser,parameter_name_parser);
         }
     }
 }
